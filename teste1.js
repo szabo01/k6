7eeb4dcef06481790c3707dcafd369dd3db15118
Robson Szabo <robinhokof@gmail.com>
2023-07-02 20:10:17 -0300

    Criando repositório com k6

---
+++ teste1.js
@@ -0,0 +1,41 @@
+import http from 'k6/http';
+import { Trend, Rate, Counter, Gauge } from 'k6/metrics';
+import { sleep } from 'k6';
+
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export function handleSummary(data) {
+  return {
+    "summary.html": htmlReport(data),
+  };
+}
+
+export const TrendRTT = new Trend('RTT');
+export const RateContentOK = new Rate('Content OK');
+export const GaugeContentSize = new Gauge('ContentSize');
+export const CounterErrors = new Counter('Errors');
+export const options = {
+  thresholds: {
+    // Count: Incorrect content cannot be returned more than 99 times.
+    'Errors': ['count<100'],
+    // Gauge: returned content must be smaller than 4000 bytes
+    'ContentSize': ['value<4000'],
+    // Rate: content must be OK more than 95 times
+    'Content OK': ['rate>0.95'],
+    // Trend: Percentiles, averages, medians, and minimums
+    // must be within specified milliseconds.
+    'RTT': ['p(99)<300', 'p(70)<250', 'avg<200', 'med<150', 'min<100'],
+  },
+};
+
+export default function () {
+  const res = http.get('https://test-api.k6.io/public/crocodiles/1/');
+  const contentOK = res.json('name') === 'Bert';
+
+  TrendRTT.add(res.timings.duration);
+  RateContentOK.add(contentOK);
+  GaugeContentSize.add(res.body.length);
+  CounterErrors.add(!contentOK);
+
+  sleep(1);
+}
